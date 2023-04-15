4c99f031bf7bc4026071304a4e4b6b48ec7291c7
dukkihong <127312253+dukkihong@users.noreply.github.com>
2023-04-15 15:14:04 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,35 @@
+for a in range(2,3):
+    for b in range(1,10):
+        print(f'{a} * {b} = {a*b:2}',end='    ')
+        print(f'{a+1} * {b} = {(a+1)*b:2}',end='    ')
+        print(f'{a+2} * {b} = {(a+2)*b:2}',end='    ')
+        print(f'{a+3} * {b} = {(a+3)*b:2}',end='    ')
+        print()
+    print()
+for c in range(6,7):
+    for d in range(1,10):
+        print(f'{c} * {d} = {c*d:2}',end='    ')
+        print(f'{c+1} * {d} = {(c+1)*d:2}',end='    ')
+        print(f'{c+2} * {d} = {(c+2)*d:2}',end='    ')
+        print(f'{c+3} * {d} = {(c+3)*d:2}',end='    ')
+        print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
